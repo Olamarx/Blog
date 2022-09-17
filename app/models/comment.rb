1714463e91d957f83f9f3076bc7b6c84593c1e12
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-17 05:55:28 +0100

    Remaining files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :posts, class_name: 'Post'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 
   after_save :update_counter
 
   private
 
   def update_counter
-    post.increment!(:commentsCounter)
+    post.increment!(:comments_counter)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :comments, foreign_key: 'author_id', inverse_of: 'author'
-  has_many :posts, foreign_key: 'author_id', inverse_of: 'author'
-  has_many :likes, foreign_key: 'author_id', inverse_of: 'author'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-  def recent_post
-    Post.where(author: self).order(created_at: :desc).first(3)
+  validates :name, presence: true, allow_blank: false
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
