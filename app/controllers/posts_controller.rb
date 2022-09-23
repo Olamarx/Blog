09e31d94b2407107245d9fd4d5d0a40d626b3f1f
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-23 07:20:02 +0100

    linter error fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts_in_pages = 2 
+    @posts_in_pages = 2
     @user = User.find(params[:user_id])
     @page = params.fetch(:pag, 1)
     @posts = @user.posts[2 * (@page.to_i - 1), @posts_in_pages]
