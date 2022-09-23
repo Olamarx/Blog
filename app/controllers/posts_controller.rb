edd35639d3d919648cd0641040ef124c50cb7674
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-23 14:39:56 +0100

    Add the current spelling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts_in_pages = 2
     @user = User.find(params[:user_id])
-    @page = params.fetch(:pag, 1)
+    @page = params.fetch(:page, 1)
     @posts = @user.posts[2 * (@page.to_i - 1), @posts_in_pages]
   end
 
