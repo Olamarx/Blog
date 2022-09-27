2b71ad0a7f86fad20a055475007a0f117fcc6844
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-28 00:21:30 +0100

    Finalized test and fix linters

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,11 +1,10 @@
-
 def create_posts_for_user(user, count: 1)
   posts = []
   count.times do |p|
     posts << Post.create!(
       author: user,
       title: "Post #{p}",
-      text: "This is description of post #{p}",
+      text: "This is description of post #{p}"
     )
   end
   posts
