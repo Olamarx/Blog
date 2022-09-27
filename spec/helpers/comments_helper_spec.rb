e1cf5a6994db79dcb289cfc6e7a26d802b0f3547
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-28 00:20:58 +0100

    Finalized test and fix linters

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,9 +1,8 @@
-
-  def create_comments_by_user(user, post, count: 1)
-    comments = []
-    count.times do |c| 
-     comment = Comment.create!(post:, author: user, text: "Coment text of #{c}")
-     comments << comment
-    end
-   comments
+def create_comments_by_user(user, post, count: 1)
+  comments = []
+  count.times do |c|
+    comment = Comment.create!(post:, author: user, text: "Coment text of #{c}")
+    comments << comment
   end
+  comments
+end
