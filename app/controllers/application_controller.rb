4a0586f2246b38f33d79b73fb39f5688e369ae98
worashf <uppert83@gmail.com>
2022-09-29 01:06:12 +0300

    Permit additional params for sign up and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+ protect_from_forgery with: :null_session
+before_action :authenticate_user!
+before_action :update_allowed_parameters, if: :devise_controller?
+
 
 protected
 
-def configure_permitted_parameters
-attrbutes = %i[name email password]
-devise_parameter_sanitizer.permit(:sign_up, keys: attrbutes)
+def update_allowed_parameters
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio,:photo, :email, :password, :password_confirmation) }
+  devise_parameter_sanitizer.permit(:account_update) do |u|
+    u.permit(:name, :surname, :email, :password, :current_password)
+  end
+end
+
+private
+
+def after_sign_out_path_for(resource_or_scope)
+new_user_session_path
 end
 end
